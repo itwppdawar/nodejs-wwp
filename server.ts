ba359fe969904822fa18c3c5aac728b69efa6f02
ariqluth <ttzluthfi@gmail.com>
2025-07-15 13:32:15 +0700

    test prod

--- server.ts
+++ server.ts
@@ -1,5 +1,10 @@
-import http, { Server } from 'http'
-import app from './src/app'
+import http, { Server } from "http";
+import app from "./src/app";
 
-const server: Server = http.createServer(app)
-server.listen(process.env.PORT || 3000, process.env.HOST || '0.0.0.0', (): void => console.log(`server is running on ${process.env.HOST || '0.0.0.0'}:${process.env.PORT || 3000}`))
+const server: Server = http.createServer(app);
+const port = parseInt(process.env.PORT || "3000", 10);
+const host = process.env.HOST || "0.0.0.0";
+
+server.listen(port, host, (): void =>
+	console.log(`server is running on ${host}:${port}`)
+);
