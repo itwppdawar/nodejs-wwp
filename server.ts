ea9cd7747a2b91676095002cb9d787d7fb7e2237
ariqluth <ttzluthfi@gmail.com>
2025-07-15 11:44:53 +0700

    coba docker production

--- server.ts
+++ server.ts
@@ -2,4 +2,4 @@ import http, { Server } from 'http'
 import app from './src/app'
 
 const server: Server = http.createServer(app)
-server.listen(process.env.PORT, (): void => console.log(`server is running on port ${process.env.PORT}`))
+server.listen(process.env.PORT || 3000, process.env.HOST || '0.0.0.0', (): void => console.log(`server is running on ${process.env.HOST || '0.0.0.0'}:${process.env.PORT || 3000}`))
